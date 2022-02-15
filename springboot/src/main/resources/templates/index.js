00fc14b4f4c29955a14702c934c894ee4ac8a9ce
noiji <enoch9@naver.com>
2022-02-15 22:25:31 +0900

    merge

--- springboot/src/main/resources/templates/index.js
+++
@@ -1,8 +0,0 @@
-const moreButton = document.querySelector(".navbar-more");
-const menu = document.querySelector(".navbar-menu");
-const icon = document.querySelector(".navbar-icon");
-
-moreButton.addEventListener("click", () => {
-  menu.classList.toggle("active");
-  icon.classList.toggle("active");
-});
